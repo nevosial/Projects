b16f3e9bb7bf9c2602e5aba9cdb3887a563d1ca9
nevosial <nevosial@gmail.com>
2017-04-01 15:32:30 +0530

    Initial index and script file

--- WorldTime/script.js
+++ WorldTime/script.js
@@ -2,23 +2,25 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function(){
-	var indiaTime = document.getElementById('current-time');
-  var londonTime = document.getElementById('current-time_b');
+
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
-	var d = new Date();
   var w = 500, h=200;
+	for(var i = 0; i<5; i++){
+		 ctx.beginPath();
+		 ctx.moveTo(Math.random()*w, Math.random()*h);
+		 ctx.lineTo(Math.random()*w, Math.random()*h);
+		 //ctx.bezierCurveTo(Math.random()*w, Math.random()*h);
+		 ctx.stroke();
+	}
 
+	var indiaTime = document.getElementById('current-time');
+	var londonTime = document.getElementById('current-time_b');
+	var d = new Date();
 	indiaTime.innerHTML = d.toTimeString();
   londonTime.innerHTML = d.toGMTString();
 
- for(var i = 0; i<200; i++){
-    ctx.beginPath();
-    ctx.moveTo(Math.random()*w, Math.random()*h);
-    ctx.lineTo(Math.random()*w, Math.random()*h);
-    //ctx.bezierCurveTo(Math.random()*w, Math.random()*h);
-    ctx.stroke();
- }
+
 
 
 });
