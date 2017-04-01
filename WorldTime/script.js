79959fef67ad85571167c245511fc78b50200cfe
nevosial <nevosial@gmail.com>
2017-04-01 19:40:01 +0530

    Maps included

--- WorldTime/script.js
+++ WorldTime/script.js
@@ -2,28 +2,60 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function(){
-
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
-  var w = 500, h=200;
-	for(var i = 0; i<5; i++){
-		 ctx.beginPath();
-		 ctx.moveTo(Math.random()*w, Math.random()*h);
-		 ctx.lineTo(Math.random()*w, Math.random()*h);
-		 //ctx.bezierCurveTo(Math.random()*w, Math.random()*h);
-		 ctx.stroke();
-	}
-
 	var indiaTime = document.getElementById('current-time');
-	var londonTime = document.getElementById('current-time_b');
+  var londonTime = document.getElementById('current-time_b');
+  //var canvas = document.getElementById('mapspot');
+
 	var d = new Date();
+
 	indiaTime.innerHTML = d.toTimeString();
   londonTime.innerHTML = d.toGMTString();
 
 
-
-
 });
-
-
 })();
+
+function initMap() {
+
+  var map = new google.maps.Map(document.getElementById('mapspot'), {
+   'center' : {lat: 12.972442, lng: 77.580643},
+  //  'center' : latlong;
+    'zoom' : 6,
+    'mapTypeId' : google.maps.MapTypeId.ROADMAP,
+    'draggable' : false,
+    'scrollwheel' : false
+  });
+  //var latlong = {lat: 12.972442, lng: 77.580643};
+  var marker = new google.maps.Marker({
+    positon: {lat: 12.972442, lng: 77.580643},
+     map: map
+  });
+
+/**
+  var infoWindow = new google.maps.InfoWindow({map: map});
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('You are here.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+***/
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+}
